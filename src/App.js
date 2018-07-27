cbab692f1714de888e7c203bb3bf802937991327
Nicholas Lepage <nic.lep@hotmail.com>
2018-07-27 21:00:15 +0000

    Connected Firestore

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line
+import * as firebase from 'firebase'
+// eslint-disable-next-line
+import db from './index.js'
+
 
 class App extends Component {
+
+  
+  //EXAMPLE OF DATABASE QUERIES
+  
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     user: "Nicholas"
+  //   };
+  // }
+  // componentDidMount() {
+  //   db.collection("users").add({
+  //     first: "Ada",
+  //     email: "ada@mail.com"
+  //   })
+  //   .then(function(docRef) {
+  //       console.log("Document written with ID: ", docRef.id);
+  //   })
+  //   .catch(function(error) {
+  //       console.error("Error adding document: ", error);
+  //   });
+  //   let usersRef = db.collection("users")
+
+  //   usersRef.get().then(function(results) {
+  //     if(results.empty) {
+  //       console.log("No documents found!");   
+  //     } else {
+  //       results.forEach(function (doc) {
+  //         console.log("Document data:", doc.data().first);
+  //       });
+  //       console.log("Document data:", results.docs[0].data());
+  //     }
+  //   }).catch(function(error) {
+  //       console.log("Error getting documents:", error);
+  //   });
+  
+  // }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1>Linia Coda</h1>
     );
   }
 }
