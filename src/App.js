ee6b1aabf238b59f3cfa0a8e723ac1565ef1c999
Ian <ian@ianUbuntu16.04>
2018-07-29 15:07:24 -0400

    Added css for User pages, finished major routing/UX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './User.css'
 // eslint-disable-next-line
 import * as firebase from 'firebase'
 // eslint-disable-next-line
@@ -8,9 +9,60 @@ import User from "./User.js";
 import Graphic from './Graphic.js';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+                  categoriesDisplay: "inline"
+                  };
+
+    // BINDING FUNCTION TO SEND AS PROPS
+    this.changeCategoriesDisplay = this.changeCategoriesDisplay.bind(this);
   }
+
+  // CHANGES STATE OF THE CATEGORY SELECTION DISPLAY
+  // STORES STATE IN SESSION STORAGE FOR PRESERVATION
+  changeCategoriesDisplay() {
+    if(this.state.categoriesDisplay === "inline") {
+      sessionStorage.setItem('categoryDisplay', 'none');
+      this.setState({categoriesDisplay: "none"});
+    }
+    if(this.state.categoriesDisplay === "none") {
+      sessionStorage.setItem('categoryDisplay', 'inline');
+      this.setState({categoriesDisplay: "inline"});
+    }
+  }
+
+   componentDidMount() {
+
+     // ENSURES ROOT WILL DISPLAY CATEGORIES TO LOGGED IN USER
+     if(window.location.pathname === "/") {
+       sessionStorage.setItem('categoryDisplay', 'inline');
+     }
+
+     // PRESERVING STATE OF CATEGORY SELECTION DISPLAY
+     let preserveState = sessionStorage.getItem('categoryDisplay');
+     this.setState({categoriesDisplay: preserveState});
+
+     document.onmouseover = function() {
+         //User's mouse is inside the page.
+         window.innerDocClick = true;
+     }
+
+     document.onmouseleave = function() {
+         //User's mouse has left the page.
+         window.innerDocClick = false;
+     }
+
+     window.onhashchange = function() {
+         if (window.innerDocClick) {
+             //Your own in-page mechanism triggered the hash change
+         } else {
+             //Browser back button was clicked
+             this.setState({categoriesDisplay: preserveState});
+         }
+     }
+   }
+
   render() {
     return (
       <div>
@@ -18,7 +70,9 @@ class App extends Component {
       <Graphic />
 
       {/* USER COMPONENT RENDERING */}
-     <User />
+     <User
+       changeCategoriesDisplay={this.changeCategoriesDisplay}
+       categoriesDisplay={this.state.categoriesDisplay}/>
       </div>
 
       )
