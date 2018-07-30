a5dce66e4b2234254e35263348ccada4a3ac8c98
Tiffany <tiffanyjdow@gmail.com>
2018-07-30 05:55:08 +0000

    got map working with google api and utilizes geolocation

---
+++ src/components/MapComponent.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { compose, withProps } from 'recompose'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+
+const MapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDgEhsdvD_39T0FWsxHIHX6H5kob8MEBwA",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+  )((props) =>
+    <GoogleMap
+      defaultZoom={14}
+      center={{ lat: props.currentLocation.lat, lng: props.currentLocation.lng }}
+    >
+      {props.isMarkerShown && <Marker position={{ lat: props.currentLocation.lat,
+        lng: props.currentLocation.lng }} onClick={props.onMarkerClick} />}
+    </GoogleMap>
+  )
+
+  export default MapComponent
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/MapContainer.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import MapComponent from './MapComponent';
+
+class MapContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentLatLng: {
+        lat: 0,
+        lng: 0
+      },
+      isMarkerShown: false
+    }
+  }
+
+  componentWillUpdate() {
+    this.getGeoLocation()
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.getGeoLocation()
+      this.setState({ isMarkerShown: true })
+    }, 5000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position.coords);
+          this.setState(prevState => ({
+            currentLatLng: {
+              ...prevState.currentLatLng,
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+         }))
+        }
+      )
+    }
+  }
+
+  render() {
+    return (
+      <MapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+        currentLocation={this.state.currentLatLng}
+      />
+    )
+  }
+}
+
+export default MapContainer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
